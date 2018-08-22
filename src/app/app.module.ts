1051456644cc4961607e9fdd7dadc0c0c650dd3a
Jonathan Wilson <jon.wilsond@gmail.com>
2018-08-21 22:54:15 -0400

    added schools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './home/home.component';
+import { SchoolComponent } from './school/school.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    HomeComponent
+    HomeComponent,
+    SchoolComponent
   ],
   imports: [
     BrowserModule
